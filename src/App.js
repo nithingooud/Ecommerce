ee0f89ab84681d087ffac5f33d582a85cacc24c4
nithin <nithin@inncircles.com>
2023-06-13 15:56:16 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Homescreen from './screens/Homescreen';
+
 const App = () => {
   return (
     <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Homescreen />
+        </Container>
+      </main>
+
     </>
   );
 }
